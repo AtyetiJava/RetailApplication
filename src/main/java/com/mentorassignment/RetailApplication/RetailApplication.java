2b44893fa1d741b775d3c445937bf2783b67d214
Vamshi Nallavalli <101942717+thevamshikrishna@users.noreply.github.com>
2025-04-23 16:01:42 +0530

    Project

---
+++ src/main/java/com/mentorassignment/RetailApplication/RetailApplication.java
@@ -0,0 +1,14 @@
+package com.mentorassignment.RetailApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailApplication.class, args);
+		System.out.println("Hii");
+	}
+
+}
